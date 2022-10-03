ec0451ba0cd2b87e8907f5b2119328f186a92dbd
Tim Buller <111061411+tbuller@users.noreply.github.com>
2022-10-03 16:05:10 +0200

    directory methods commit

--- directory.rb
+++ directory.rb
@@ -12,11 +12,23 @@ students = [
 "Joffrey Baratheon",
 "Norman Bates"
 ]
-# and then print them
-puts "The students of Villain Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+
+def print_header
+  puts "The students of Villain Academy"
+  puts "-------------"
+end  
+
+def print(names)
+  names.each do |name|
+    puts name
+  end  
 end    
-# finally we print the total
-puts "Overall, we have #{students.count} great students"
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
